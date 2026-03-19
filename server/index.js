94bae8829bce03ccbf62d2ce44ec5e9a8cb6c4bd
SnehaRao31078 <sneharao31078@gmail.com>
2026-03-19 18:46:13 +0530

    switch to vercel

--- server/index.js
+++ server/index.js
@@ -27,7 +27,9 @@ let otpStore = {};
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.APP_PASSWORD,
