e5f04da3f29ae5d812d94a4075e256b1a305611d
SnehaRao31078 <sneharao31078@gmail.com>
2026-03-27 10:17:03 +0530

    fix vercel routing

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./signuppass.css";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 
 function Signup() {
   console.log("API URL:", import.meta.env.VITE_API_URL);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-const  navigate=useNavigate();  
+ 
   /*const handleSubmit = (e) => {
   e.preventDefault();
 
