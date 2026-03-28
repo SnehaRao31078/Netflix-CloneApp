ee63985912d6f702b4366981034f7c2c0ca793a0
SnehaRao31078 <sneharao31078@gmail.com>
2026-03-28 14:09:15 +0530

    planform

--- client/src/Addproduct.jsx
+++ client/src/Addproduct.jsx
@@ -1,63 +1,78 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useParams, useLocation } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import "./product.css";
 
-function Plan() {
+function Addproduct() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const location = useLocation();
 
-  const { plan, price } = location.state || {};
+  const [file, setFile] = useState(null);
+  const [video, setVideo] = useState(null);
 
-  const [email, setEmail] = useState("");
-  const [card, setCard] = useState("");
-  const [holder, setHolder] = useState("");
-  const [country, setCountry] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [language, setLanguage] = useState("");
+  const [category, setCategory] = useState("");
+  const [plan, setPlan] = useState("");
 
+  const [imagePreview, setImagePreview] = useState("");
+  const [videoPreview, setVideoPreview] = useState("");
 
   useEffect(() => {
     if (id) {
       axios
-        .get(`${import.meta.env.VITE_API_URL}/plans/${id}`)
+        .get(`${import.meta.env.VITE_API_URL}/products/${id}`)
         .then((res) => {
-          setEmail(res.data.email);
-          setCard(res.data.card);
-          setHolder(res.data.holder);
-          setCountry(res.data.country);
+          setTitle(res.data.title);
+          setDescription(res.data.description);
+          setLanguage(res.data.language);
+          setCategory(res.data.category);
+          setPlan(res.data.plan);
+          setImagePreview(res.data.file);
+          setVideoPreview(res.data.video);
         })
         .catch((err) => console.log(err));
     }
   }, [id]);
 
-  
+  const removeImage = () => {
+    setImagePreview("");
+    setFile(null);
+  };
+
+  const removeVideo = () => {
+    setVideoPreview("");
+    setVideo(null);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const data = {
-      email,
-      card,
-      holder,
-      country,
-      plan,
-      price,
-    };
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("language", language);
+    formData.append("category", category);
+    formData.append("plan", plan);
+
+    if (file) formData.append("file", file);
+    if (video) formData.append("video", video);
 
     try {
       if (id) {
         await axios.put(
-          `${import.meta.env.VITE_API_URL}/plans/${id}`,
-          data
+          `${import.meta.env.VITE_API_URL}/products/${id}`,
+          formData,
         );
         alert("Updated Successfully");
       } else {
-        await axios.post(
-          `${import.meta.env.VITE_API_URL}/plans`,
-          data
-        );
+        await axios.post(`${import.meta.env.VITE_API_URL}/products`, formData);
         alert("Added Successfully");
       }
 
-      navigate("/home");
+      navigate("/view");
     } catch (err) {
       console.log(err);
     }
@@ -67,41 +82,90 @@ function Plan() {
     <div className="product-page">
       <div className="product-wrapper">
         <div className="container">
-          <h1>{id ? "Update Plan" : "Add Plan"}</h1>
+          <h1>{id ? "Update Movie" : "Add Movie"}</h1>
 
           <form onSubmit={handleSubmit}>
             <input
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
 
             <input
-              placeholder="Card Details"
-              value={card}
-              onChange={(e) => setCard(e.target.value)}
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
 
             <input
-              placeholder="Card Holder Name"
-              value={holder}
-              onChange={(e) => setHolder(e.target.value)}
+              placeholder="Language"
+              value={language}
+              onChange={(e) => setLanguage(e.target.value)}
             />
+            <div>
+              <select
+                className="inputfield"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Select Category</option>
+                <option value="made in india">Made in India</option>
+                <option value="sci-fi horror">Sci-Fi Horror</option>
+              </select>
+            </div>
 
             <div>
               <select
                 className="inputfield"
-                value={country}
-                onChange={(e) => setCountry(e.target.value)}
+                value={plan}
+                onChange={(e) => setPlan(e.target.value)}
               >
-                <option value="">Select Country</option>
-                <option value="india">India</option>
+                <option value="">Select Plan</option>
+                <option value="basic">Basic</option>
+                <option value="Standard">Standard</option>
+                <option value="premium">Premium</option>
               </select>
             </div>
 
-            <button type="submit">
-              {id ? "Update Plan" : "Add Plan"}
-            </button>
+            <input
+              type="file"
+              name="file"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+
+            {imagePreview && (
+              <div style={{ position: "relative", display: "inline-block" }}>
+                <img
+                  src={`${import.meta.env.VITE_API_URL}/Images/${imagePreview}`}
+                  width="120"
+                  alt="preview"
+                />
+                <p className="cross" onClick={removeImage}>
+                  X
+                </p>
+              </div>
+            )}
+
+            <input
+              type="file"
+              name="video"
+              onChange={(e) => setVideo(e.target.files[0])}
+            />
+
+            {videoPreview && (
+              <div style={{ position: "relative", display: "inline-block" }}>
+                <video
+                  src={`${import.meta.env.VITE_API_URL}/Images/${videoPreview}`}
+                  width="120"
+                  controls
+                />
+                <p className="cross" onClick={removeVideo}>
+                  X
+                </p>
+              </div>
+            )}
+
+            <button type="submit">{id ? "Update Movie" : "Add Movie"}</button>
           </form>
         </div>
       </div>
@@ -109,4 +173,4 @@ function Plan() {
   );
 }
 
-export default Plan;
+export default Addproduct;
