9a34051277799ee82d0c879d6454e734140dd9b7
SnehaRao31078 <sneharao31078@gmail.com>
2026-03-29 10:18:38 +0530

    planform

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,93 +1,107 @@
- import { Link } from "react-router-dom";
- import "./home.css";
- import { useState, useEffect } from "react";
- function Nav() {
+import { Link, useNavigate } from "react-router-dom";
+import "./home.css";
+import { useState, useEffect } from "react";
+function Nav() {
   const [user, setUser] = useState("");
   useEffect(() => {
-  const email = localStorage.getItem("userEmail");
-  if (email) {
-    setUser(email);
-  }
-}, []);
- return (
-  
- <nav className="navbar">
-        <div className="nav-left">
-          <div className="logo">NETFLIX</div>
+    const email = localStorage.getItem("userEmail");
+    if (email) {
+      setUser(email);
+    }
+  }, []);
+  const navigate = useNavigate();
 
-          <ul className="nav-links">
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/shows">Shows</Link></li>
-            <li><Link to="/movies">Movies</Link></li>
-            <li><Link to="/games">Games</Link></li>
-            <li><Link to="/new">New & Popular</Link></li>
-            <li><Link to="/mylist">My List</Link></li>
-            <li><Link to="/languages">Browse by Languages</Link></li>
-          </ul>
-        </div>
+  const handleLogout = () => {
+    localStorage.removeItem("userEmail");
+    localStorage.removeItem("userPlan");
 
-        <div className="nav-right">
-          <i className="fas fa-search icon"></i>
-          <span>Children</span>
-          <i className="fas fa-bell icon"></i>
-         
-    <div className="profile">
-      <img src="/profile.png" className="top-profile"/>
+    navigate("/");
+  };
+  return (
+    <nav className="navbar">
+      <div className="nav-left">
+        <div className="logo">NETFLIX</div>
 
-    
-      <div className="profile-menu">
-
-        <div className="menu-row">
-          <img src="/redsmile.png"/>
-          <span>{user}</span>
-        </div>
-
-        
-
-        <div className="menu-row">
-          <img src="/kids.png"/>
-          <span>Kids</span>
-        </div>
-
-        <div className="divider"></div>
-
-        <div className="menu-row small">
-         <i className="fa-regular fa-pen-to-square"></i>
-
-          <span>Manage Profiles</span>
-        </div>
-
-        <div className="menu-row small">
-         <i className="fa-solid fa-right-left"></i>
-
-          <span>Transfer Profile</span>
-        </div>
-
-        <div className="menu-row small">
-          <i className="fa-regular fa-user"></i>
-
-          <span>Account</span>
-        </div>
-
-        <div className="menu-row small">
-          <i className="fa-regular fa-circle-question"></i>
-
-          <span>Help Centre</span>
-        </div>
-
-        <div className="divider"></div>
-
-        <div className="signout">
-          Sign out of Netflix
-        </div>
-
+        <ul className="nav-links">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/shows">Shows</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+          <li>
+            <Link to="/games">Games</Link>
+          </li>
+          <li>
+            <Link to="/new">New & Popular</Link>
+          </li>
+          <li>
+            <Link to="/mylist">My List</Link>
+          </li>
+          <li>
+            <Link to="/languages">Browse by Languages</Link>
+          </li>
+        </ul>
       </div>
-     
-    </div>
 
-  
+      <div className="nav-right">
+        <i className="fas fa-search icon"></i>
+        <span>Children</span>
+        <i className="fas fa-bell icon"></i>
+
+        <div className="profile">
+          <img src="/profile.png" className="top-profile" />
+
+          <div className="profile-menu">
+            <div className="menu-row">
+              <img src="/redsmile.png" />
+              <span>{user}</span>
+            </div>
+
+            <div className="menu-row">
+              <img src="/kids.png" />
+              <span>Kids</span>
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="menu-row small">
+              <i className="fa-regular fa-pen-to-square"></i>
+
+              <span>Manage Profiles</span>
+            </div>
+
+            <div className="menu-row small">
+              <i className="fa-solid fa-right-left"></i>
+
+              <span>Transfer Profile</span>
+            </div>
+
+            <div className="menu-row small">
+              <i className="fa-regular fa-user"></i>
+
+              <span>Account</span>
+            </div>
+
+            <div className="menu-row small">
+              <i className="fa-regular fa-circle-question"></i>
+
+              <span>Help Centre</span>
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="signout" onClick={handleLogout}>
+              Sign out of Netflix
+            </div>
+          </div>
         </div>
-      </nav>
- )}
+      </div>
+    </nav>
+  );
+}
 
 export default Nav;
