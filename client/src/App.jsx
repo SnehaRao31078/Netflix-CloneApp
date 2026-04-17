f944d8041836d44ef54b28e311f358ff6906fae0
SnehaRao31078 <sneharao31078@gmail.com>
2026-04-17 10:46:35 +0530

    ro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
         <Route path="/plan" element={<Plan />} />
 
         <Route path="/subview" element={<ViewScription />} />
-        <Route path="/panel" element={<Userpanel />} />
+        <Route path="/panel" element={<UserDash />} />
         <Route path="/security" element={<Security />} />
         <Route path="/membership" element={<Membership />} />
 
