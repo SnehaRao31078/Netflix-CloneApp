ffd4075b412aabc6fee0030ca70d3f539855dbc1
SnehaRao31078 <sneharao31078@gmail.com>
2026-04-23 10:44:39 +0530

    pie

---
+++ client/src/Charts.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import {Pie,PieChart,Cell,ToolTip, ResponsiveContainer} from "recharts";
+import axios from "axios";
+function Charts(){
+    const [data,setData]=useState([]);
+   useEffect(()=>
+{
+const fetchedata =async ()=>
+{
+    try
+    {
+       const res=await axios.get(`${import.meta.env.VITE_API_URL}/plans`);
+       const FormattedData=res.data.map(item=>
+       ({
+          name:item.plan,
+          value:item.price
+
+       })
+       )
+       setData(FormattedData);
+
+    }
+    catch(error)
+    {
+     console.error("Error Fetching MongoDB data")   
+    }
+};
+fetchedta();
+
+},[]);
+
+const COLORS=["#0088FE","#00C49F","#FF8828"];
+ return(
+<>
+<ResponsiveContainer>
+<PieChart width={200} height={200}>
+<Pie data={data}  value="plan"
+cx="50%"
+cy="50%"
+outerRadius={100}
+fill="#8884d8"
+label
+>
+    {data.map((entry,index)=>
+    {
+        <Cell key={`cell-${index}`}
+        fill={COLORS[index%COLORS.length]}></Cell>
+    })}
+    <Tooltip></Tooltip>
+
+
+</Pie>
+</PieChart>
+</ResponsiveContainer>
+</>
+    );
+}
+export default Charts;
